f6a6c228398af63ef692ea094ec7ede69bf73b8b
3rumalarao <3rumalarao@gmail.com>
2025-04-19 10:59:34 -0500

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,12 @@
+output "private_instance_ids" { value = module.private_ec2.instance_ids }
+output "private_instance_ips" { value = module.private_ec2.private_ips }
+output "public_instance_ids"  { value = module.public_ec2.instance_ids }
+output "public_instance_ips"  { value = module.public_ec2.public_ips }
+output "efs_id"              { value = module.efs.efs_id }
+output "crm_lb_dns"          { value = module.crm_lb.lb_dns }
+output "clover_lb_dns"       { value = module.clover_lb.lb_dns }
+output "ldaphaproxy_lb_dns" { value = module.ldaphaproxy_lb.lb_dns }
+output "rds_endpoint"        { value = module.rds.rds_endpoint }
+output "ssm_parameters"      { value = module.ssm.ssm_parameters }
+output "backup_plan_id"      { value = length(module.backup)>0?module.backup[0].backup_plan_id:"" }
+output "sg_ids"              { value = module.sg.sg_ids }
