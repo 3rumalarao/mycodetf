58494c1941d800bcd46c58844627a37903a1db38
3rumalarao <3rumalarao@gmail.com>
2025-04-20 15:25:59 -0500

    Update outputs.tf

--- backup/outputs.tf
+++ backup/outputs.tf
@@ -1,3 +1,14 @@
 output "backup_plan_id" {
-  value = aws_backup_plan.this.id
+  description = "The ID of the AWS Backup plan created." # Added description for clarity
+  value       = aws_backup_plan.this.id
+}
+
+output "backup_vault_name" {
+  description = "The name of the AWS Backup vault created."
+  value       = aws_backup_vault.this.name
+}
+
+output "backup_vault_arn" {
+  description = "The ARN of the AWS Backup vault created."
+  value       = aws_backup_vault.this.arn
 }
