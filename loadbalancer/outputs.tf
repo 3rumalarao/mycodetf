b2bc2ce4a2d8cb32f40fbeccbc0e6c4229b5c1c2
3rumalarao <3rumalarao@gmail.com>
2025-04-20 15:31:56 -0500

    Update outputs.tf

--- loadbalancer/outputs.tf
+++ loadbalancer/outputs.tf
@@ -1,7 +1,29 @@
 output "lb_dns" {
-  value = aws_lb.this.dns_name
+  description = "DNS name of the created Load Balancer."
+  value       = aws_lb.this.dns_name
+}
+
+output "lb_arn" {
+  description = "ARN of the created Load Balancer."
+  value       = aws_lb.this.arn
+}
+
+output "lb_zone_id" {
+  description = "Route 53 Hosted Zone ID for the Load Balancer (useful for alias records)."
+  value       = aws_lb.this.zone_id
 }
 
 output "target_group_arn" {
-  value = aws_lb_target_group.this.arn
+  description = "ARN of the created Target Group."
+  value       = aws_lb_target_group.this.arn
+}
+
+output "target_group_name" {
+  description = "Name of the created Target Group."
+  value       = aws_lb_target_group.this.name
+}
+
+output "listener_arn" {
+  description = "ARN of the created Listener."
+  value       = aws_lb_listener.this.arn
 }
