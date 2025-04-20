66e821ba3accf897ab5db901703d65bcef121324
3rumalarao <3rumalarao@gmail.com>
2025-04-20 15:34:51 -0500

    Update outputs.tf

--- sg/outputs.tf
+++ sg/outputs.tf
@@ -1,4 +1,4 @@
 output "sg_ids" {
-  description = "Map of SG IDs keyed by definition key"
+  description = "Map of logical security group names (keys from input variable) to their AWS IDs."
   value       = { for k, sg in aws_security_group.this : k => sg.id }
 }
