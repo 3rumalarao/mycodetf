e50027caf04ebeb17d78e9f9ea0756cd8788cb7a
3rumalarao <3rumalarao@gmail.com>
2025-04-20 15:30:14 -0500

    Update outputs.tf

--- efs/outputs.tf
+++ efs/outputs.tf
@@ -1,3 +1,15 @@
 output "efs_id" {
-  value = aws_efs_file_system.this.id
+  description = "The ID of the created EFS file system."
+  value       = aws_efs_file_system.this.id
 }
+
+output "efs_arn" {
+  description = "The ARN of the created EFS file system."
+  value       = aws_efs_file_system.this.arn
+}
+
+# Optional: Output mount target IPs if needed
+# output "mount_target_ips" {
+#   description = "Map of Availability Zone to Mount Target IP Address."
+#   value       = { for az, mt in aws_efs_mount_target.this : az => mt.ip_address }
+# }
