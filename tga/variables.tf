31341157e0db5832a93870eba74b5fa03ef8f8c5
3rumalarao <3rumalarao@gmail.com>
2025-04-20 15:37:37 -0500

    Update variables.tf

--- tga/variables.tf
+++ tga/variables.tf
@@ -1,2 +1,13 @@
-variable "target_group_arn" { type = string }
-variable "targets" { type = list(object({ id:string, port:number })) }
+variable "target_group_arn" {
+  description = "ARN of the Target Group to which targets will be registered."
+  type        = string
+}
+
+variable "targets" {
+  description = "List of target objects to register. Each object must have 'id' (instance ID, IP address, etc.) and 'port'."
+  type        = list(object({
+    id   = string
+    port = number
+  }))
+  default = [] # Default to empty list to avoid errors if no targets are provided
+}
